e68ec8b7461602c318e925043191a6a016099ccb
Gemma2008 <114920831+Gemma2008@users.noreply.github.com>
2023-04-12 13:46:13 +0200

    Create 12 abril robot.py

---
+++ robot 12/12 abril robot.py
@@ -0,0 +1,18 @@
+# Imports go at the top
+from microbit import *
+from MicroRover import*
+
+robot = Micro_Rover()
+
+while True:
+    distancia = robot.get_distance()
+    display.show(distancia)
+    if distancia >= 10:
+        robot.motor(255,255)
+    else:
+        robot.motor(0,0)
+
+
+   
+        
+   
